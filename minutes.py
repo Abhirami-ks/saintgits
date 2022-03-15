74b43a93b3f0c6c0605d3aee67068d0be8fc7789
Abhirami-ks <95563746+Abhirami-ks@users.noreply.github.com>
2022-03-15 10:29:02 +0530

    Add files via upload

---
+++ minutes.py
@@ -0,0 +1,15 @@
+#print the number of minutes in a year.
+print('-----------number of minutes in a year-------------')
+m_h = 60
+print('Number of minutes in a hour is ',m_h)
+print('Number of hours in a day is 24')
+m_day = m_h * 24
+m_year = m_day * 365
+print('Number of minutes in a day is ',m_year)
+
+
+
+
+
+
+
